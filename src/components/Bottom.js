67bfeff413ea7940a7e7c0d44e4febc1d4f97c01
Christoffer Frydkjær <cfk@ChristoffersMBP.local>
2021-02-25 19:57:01 +0100

    added github logo and fix responsive design

--- src/components/Bottom.js
+++ src/components/Bottom.js
@@ -1,15 +1,22 @@
 import linkedin from './../assets/linkedin.svg'
+import github from './../assets/github.svg'
 
 import Obfuscate from 'react-obfuscate';
 
 function Bottom() {
 return (
     <div class="grid grid-cols-3 bg-gray-800 min-h-screen text-center m-auto text-white py-20 font-thin">
-        <div class="col-start-2">
-            <div class="border-b-2 border-solid w-40 m-auto border-primary mb-5">
-                <a href="https://www.linkedin.com/in/christoffer-kristensen-73131261/" target="_blank" ><img class="w-7 m-auto mb-5" src={linkedin}></img></a>
+        <div class="col-start-2 flex flex-col">
+            <div class="border-b-2 border-solid w-40 place-self-center border-primary">
+                <div class="mb-5 inline-flex">
+                    <a href="https://www.linkedin.com/in/christoffer-kristensen-73131261/" target="_blank" ><img class="w-7 mr-5" src={linkedin}></img></a>
+                    <a href="https://github.com/ckr123" target="_blank"><img class="w-7" src={github}></img></a>
+                </div>
             </div>
-            <Obfuscate email="christoffer@nelu.dk"></Obfuscate>
+            <div>
+                <Obfuscate email="christoffer@nelu.dk"></Obfuscate>    
+            </div>
+            
         </div>
     </div>
 );
--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -3,9 +3,9 @@ import logo from './../assets/logo.jpeg'
 
 function Topbar() {
 return (
-    <div class="grid grid-rows-1 md:grid-cols-3 gap-4 bg-white items-baseline">
+    <div class="grid grid-rows-1 lg:grid-cols-3 gap-4 bg-white items-baseline">
         <div class="relative md:pl-60 col-span-2 ">
-            <div class="top-1/2 left-32 text-right max-w-sm m-10 md:m-0 md:absolute">
+            <div class="top-1/2 left-32 text-right max-w-sm m-10 lg:m-0 lg:absolute">
                 <div class="text-4xl font-thin">
                     <p class="">CHRISTOFFER</p>
                     <p class="border-b-2 border-solid border-primary">FRYDKJÆR</p>
@@ -15,7 +15,7 @@ return (
             </div>
             <img class="md:max-w-md md:m-0" src={Christoffer} />
         </div>
-        <div class="md:col-start-3 col-span-2 m-auto xl:-ml-20 md:-ml-10 md:mt-32">
+        <div class="lg:col-start-3 col-span-2 m-auto xl:-ml-20 lg:-ml-10 lg:mt-32">
             <div class="max-w-xs ">
                 <p class="">Skal jeg hjælpe dig?</p>
                 <p class="text-sm font-thin">
